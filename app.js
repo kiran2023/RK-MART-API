63b695c0b04d74627b0e6e4869a5bc8edd9b59b9
KIRAN <88279441+kiran2023@users.noreply.github.com>
2023-11-25 15:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const products_route = require('./routes/productRoutes');
 
-app.get('/', (request, response)=>{
-    response.status(200).send({
-        MESSAGE: "STARTED"
-    })
-});
-
-app.use('/api/products', products_route);
+app.use('/', products_route);
 
 module.exports = app;
