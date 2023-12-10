f72f14ce22bf7da699bb014f92f6a715677362a1
KIRAN <88279441+kiran2023@users.noreply.github.com>
2023-12-10 21:40:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const cors = require('cors');
+
 const express = require('express');
 const app = express();
 const products_route = require('./routes/productRoutes');
+
+var corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET, POST, PUT, PATCH, DELETE, HEAD',
+    credentials: true,
+}
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use('/', products_route);
 
